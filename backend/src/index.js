51b8c31051961c184f026eb225c4b781220ea6ae
AMAR  SINHA <sinhaamar178@gmail.com>
2026-08-03 02:04:18 +0530

    ready for deploy may be

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import fs from "fs";
+import path from "path";
 
 import { clerkMiddleware } from '@clerk/express';
 
@@ -12,7 +14,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-
+const publicDir = path.join(process.cwd(), "public");
 
 
 //console.log(process.env.DB_URL);
@@ -24,6 +26,14 @@ app.get("/health", (req, res) => {
     res.status(200).json({ ok: true});
 
 });
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+
+    app.get("/{*any}",(req,res,next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    })
+}
+
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is up and running on port PORT",PORT);
