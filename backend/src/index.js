fa6177cf250098354f30943df544c455fadefe23
AMAR  SINHA <sinhaamar178@gmail.com>
2026-08-03 00:32:59 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+//const express = require("express")
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+const PORT = process.env.PORT
+
+console.log(process.env.DB_URL);
+
+
+app.listen(PORT, () => console.log("Server is up and running on port PORT",PORT));
