fad30c008197d1cf82d911925ca1d32388332822
AMAR  SINHA <sinhaamar178@gmail.com>
2026-09-01 10:08:53 +0530

    message composer fixed

--- frontend/src/components/chat/ChatComposer.jsx
+++ frontend/src/components/chat/ChatComposer.jsx
@@ -78,7 +78,7 @@ export function ChatComposer() {
         <TextArea
           fullWidth
           variant="secondary"
-          placeholder="iMessage"
+          placeholder="Type your message"
           rows={1}
           value={composerText}
           onChange={handleComposerTextChange}
